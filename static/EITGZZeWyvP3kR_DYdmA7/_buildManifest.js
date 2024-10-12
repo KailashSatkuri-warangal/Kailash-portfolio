959069456f091804e9b436ac63c6cf5014a4e3de
Satkuri Kailash <satkurikailash@gmail.com>
2024-10-12 08:27:29 +0530

    add

---
+++ static/EITGZZeWyvP3kR_DYdmA7/_buildManifest.js
@@ -0,0 +1,11 @@
+self.__BUILD_MANIFEST = {
+    __rewrites: {
+        beforeFiles: [],
+        afterFiles: [],
+        fallback: []
+    },
+    "/": ["static/chunks/d0447323-1b6cc5198abea4fe.js", "static/chunks/1bfc9850-35c77360987ffbeb.js", "static/chunks/210-3296d5771ab2587e.js", "static/css/0efa9c65e1afd0c2.css", "static/chunks/pages/index-a345638618cc987e.js"],
+    "/_error": ["static/chunks/pages/_error-2280fa386d040b66.js"],
+    sortedPages: ["/", "/_app", "/_error"]
+},
+self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB();
