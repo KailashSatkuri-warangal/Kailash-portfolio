13ebbe94e0570a330ec36e3196cc09e8e397c5ba
KailashSatkuri-warangal <satkurikailash@gmail.com>
2025-05-11 18:35:14 +0530

    github icon remove pateron

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "a8fe95d41cfb54b0"; }
+/******/ 		__webpack_require__.h = function() { return "b3acf63af40ed831"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
