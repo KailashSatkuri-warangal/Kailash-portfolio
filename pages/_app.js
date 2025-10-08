246d8edc7a21636f5244e6ea5b0157ba65c4ed02
Satkuri Kailash <84554842+KailashSatkuri-warangal@users.noreply.github.com>
2025-10-08 21:18:09 +0530

    adsence

--- pages/_app.js
+++ pages/_app.js
@@ -3,9 +3,17 @@ import CookieBanner from '../components/CookieBanner/CookieBanner';
 import Particles from '../components/Particles/Particles';
 import particlesConfig from '../utils/particles.json';
 import '@fortawesome/fontawesome-free/css/all.min.css';
+import Head from "next/head";
 function MyApp({ Component, pageProps }) {
   return (
     <>
+    <Head>
+				<script
+					async
+					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9872384661755732"
+					crossOrigin="anonymous"
+				></script>
+			</Head>
       <Particles options={particlesConfig} />
       <Component {...pageProps} />
       <CookieBanner />
