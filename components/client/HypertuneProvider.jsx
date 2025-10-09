bd1e93a187eb52f39161aed50d8dcaf3d31a338d
Satkuri Kailash <84554842+KailashSatkuri-warangal@users.noreply.github.com>
2025-10-09 22:05:08 +0530

    Hypertune 5.0

--- components/client/HypertuneProvider.jsx
+++ components/client/HypertuneProvider.jsx
@@ -5,21 +5,21 @@ import getHypertune from '../../lib/getHypertune.client';
 export const HypertuneContext = createContext({});
 
 export default function AppHypertuneProvider({ children }) {
-	const [flags, setFlags] = useState(null);
+  const [flags, setFlags] = useState(null);
 
-	useEffect(() => {
-		async function fetchFlags() {
-			const hypertuneFlags = await getHypertune();
-			setFlags(hypertuneFlags);
-		}
-		fetchFlags();
-	}, []);
+  useEffect(() => {
+    async function fetchFlags() {
+      const hypertuneFlags = await getHypertune();
+      setFlags(hypertuneFlags);
+    }
+    fetchFlags();
+  }, []);
 
-	if (!flags) return <div>Loading portfolio...</div>;
+  if (!flags) return <div>Loading flags...</div>;
 
-	return (
-		<HypertuneContext.Provider value={flags}>
-			{children}
-		</HypertuneContext.Provider>
-	);
+  return (
+    <HypertuneContext.Provider value={flags}>
+      {children}
+    </HypertuneContext.Provider>
+  );
 }
