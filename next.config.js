b8acae6297ab3d46a38064413df18fa83bf1dddb
Satkuri Kailash <84554842+KailashSatkuri-warangal@users.noreply.github.com>
2025-10-09 19:05:49 +0530

    Hypertune

--- next.config.js
+++ next.config.js
@@ -4,6 +4,10 @@ const nextConfig = {
 	images: {
 		domains: ['avatars.githubusercontent.com'], // allow GitHub avatars
 	},
+	env: {
+		EXPERIMENTATION_EXPERIMENTATION_CONFIG_ITEM_KEY: process.env.EXPERIMENTATION_EXPERIMENTATION_CONFIG_ITEM_KEY,
+		EXPERIMENTATION_NEXT_PUBLIC_HYPERTUNE_TOKEN: process.env.EXPERIMENTATION_NEXT_PUBLIC_HYPERTUNE_TOKEN
+	}
 };
 
 module.exports = nextConfig;
--- pages/_app.js
+++ pages/_app.js
@@ -1,24 +1,33 @@
-import '../styles/globals.css';
-import CookieBanner from '../components/CookieBanner/CookieBanner';
-import Particles from '../components/Particles/Particles';
-import particlesConfig from '../utils/particles.json';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import Head from "next/head";
+import '../styles/globals.css'
+import CookieBanner from '../components/CookieBanner/CookieBanner'
+import Particles from '../components/Particles/Particles'
+import particlesConfig from '../utils/particles.json'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import Head from "next/head"
+import { useEffect } from "react"
+
 function MyApp({ Component, pageProps }) {
-  return (
-    <>
-    <Head>
+
+	useEffect(() => {
+		console.log("Hypertune Connected ✅")
+		console.log("Config Key:", process.env.EXPERIMENTATION_EXPERIMENTATION_CONFIG_ITEM_KEY)
+		console.log("Token:", process.env.EXPERIMENTATION_NEXT_PUBLIC_HYPERTUNE_TOKEN)
+	}, [])
+
+	return (
+		<>
+			<Head>
 				<script
 					async
 					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9872384661755732"
 					crossOrigin="anonymous"
 				></script>
 			</Head>
-      <Particles options={particlesConfig} />
-      <Component {...pageProps} />
-      <CookieBanner />
-    </>
-  );
+			<Particles options={particlesConfig} />
+			<Component {...pageProps} />
+			<CookieBanner />
+		</>
+	)
 }
 
-export default MyApp;
+export default MyApp
