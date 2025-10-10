8720625778ed15cb46ccb505cd6f5229af611be4
Satkuri Kailash <84554842+KailashSatkuri-warangal@users.noreply.github.com>
2025-10-10 18:55:22 +0530

    adsence

--- pages/_app.js
+++ pages/_app.js
@@ -1,27 +1,37 @@
-import '../styles/globals.css';
-import CookieBanner from '../components/CookieBanner/CookieBanner';
-import Particles from '../components/Particles/Particles';
-import particlesConfig from '../utils/particles.json';
-import '@fortawesome/fontawesome-free/css/all.min.css';
-import Head from 'next/head';
-import AppHypertuneProvider from '../components/client/HypertuneProvider';
+import '../styles/globals.css'
+import CookieBanner from '../components/CookieBanner/CookieBanner'
+import Particles from '../components/Particles/Particles'
+import particlesConfig from '../utils/particles.json'
+import '@fortawesome/fontawesome-free/css/all.min.css'
+import Head from 'next/head'
+import AppHypertuneProvider from '../components/client/HypertuneProvider'
+import { useRouter } from 'next/router'
 
 function MyApp({ Component, pageProps }) {
+	const router = useRouter()
+
+	// Define routes where ads should NOT load
+	const excludedRoutes = ['/loading', '/404', '/maintenance']
+
+	const shouldShowAds = !excludedRoutes.includes(router.pathname)
+
 	return (
 		<AppHypertuneProvider>
 			<Head>
-				<script
-					async
-					src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9872384661755732"
-					crossOrigin="anonymous"
-				/>
+				{shouldShowAds && (
+					<script
+						async
+						src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9872384661755732"
+						crossOrigin="anonymous"
+					/>
+				)}
 			</Head>
 
 			<Particles options={particlesConfig} />
 			<Component {...pageProps} />
 			<CookieBanner />
 		</AppHypertuneProvider>
-	);
+	)
 }
 
-export default MyApp;
+export default MyApp
